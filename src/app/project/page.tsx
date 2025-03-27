2b078872fbd327592c65fae073a86203981c9a57
TomDevy <agboolaemmanuel524@gmail.com>
2025-03-27 13:01:49 +0100

    change project urls

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -7,35 +7,35 @@ const projects = [
   {
     title: "Vesspor - Real Estate Investment Platform",
     description: "A modern platform enabling individuals to invest in real estate properties seamlessly.",
-    image: "https://pikwy.com/web/67e538ed0aed1225a07e6b5f",
+    image: "/vesspor.jpg",
     githubUrl: "", // Add link if you have the repo public, or leave empty
     liveUrl: "https://vesspor.com"
   },
   {
     title: "Olamide.ng - Digital Artist Portfolio",
     description: "A beautifully crafted portfolio showcasing the stunning works of a talented digital artist.",
-    image: "https://pikwy.com/web/67e538012bd4367d7671e31e",
+    image: "olamide.jpg",
     githubUrl: "", // Add link if you have the repo public
     liveUrl: "https://olamide.ng"
   },
   {
     title: "Inkoret - Digital Artist Potfolio and Store",
     description: "A portfolio website and store for a digital artist",
-    image: "https://pikwy.com/web/67e538bdfa5923757668afca",
+    image: "inkoret.jpg",
     githubUrl: "", // Add link if you have the repo public
     liveUrl: "https://inkoret.vercel.app"
   },
   {
     title: "OA Stores - E-commerce for Digital Arts & Materials",
     description: "An e-commerce website offering digital arts and creative materials for artists and enthusiasts.",
-    image: "https://pikwy.com/web/67e5384a4f92a71c5f2c0062",
+    image: "oastores.jpg",
     githubUrl: "", // Add link if you have the repo public
     liveUrl: "https://oastores.com"
   },
   {
     title: "Blaze Brand Limited - Migration Agency Website",
     description: "An informational website providing guidance and services for individuals looking to migrate abroad.",
-    image: "https://pikwy.com/web/67e53881a778631628447913",
+    image: "blazebrandlimited.jpg",
     githubUrl: "", // Add link if you have the repo public
     liveUrl: "https://blazebrandlimited.com"
   }
