c6d192cc2d2ed558172375a617d8c61902da60e5
tomdevy <agboolaemmanuel524@gmail.com>
2026-06-02 11:39:46 +0100

    update CV

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -85,7 +85,7 @@ export default function Hero() {
         </div>
 
         <Button variant="outline" size="lg" asChild>
-          <a href="/Emmanuel_Agboola_CV.pdf" download>
+          <a href="/Emmanuel_Agboola_CV1.pdf" download>
             Download Resume
           </a>
         </Button>
