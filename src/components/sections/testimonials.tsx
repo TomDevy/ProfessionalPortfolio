63473fe5419bf9be0d6831f0941913a8c565be5b
tomdevy <agboolaemmanuel524@gmail.com>
2026-06-11 11:05:46 +0100

    update testimonials

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -44,7 +44,7 @@ const testimonials = [
   },
   {
     name: "Sam Obadina",
-    role: "Software Engineer",
+    role: "Founder, Feathers Creative",
     image: "/stan.jpg",
     feedback:
       "Working with Emmanuel was a great experience. He’s a skilled frontend developer with a strong grasp of Next.js and a keen eye for detail. Beyond his technical abilities, he’s a friendly and approachable team player who collaborates effectively and always brings a problem-solving mindset to the table. His positive attitude made working together smooth and productive. I highly recommend him for any team looking for a reliable and talented developer.",
